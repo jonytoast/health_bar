bfdcd4607e8e97fcf0910ed261429a365aa51d46
FeddericoWayne <jackiew1120@hotmail.com>
2023-02-23 11:46:45 -0500

    added to connection.js and Model directory with sequelize Models and index file containing Model associations

---
+++ config/connection.js
@@ -0,0 +1,30 @@
+// Sequelize database connection process
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+let sequelize;
+
+// when using JAWSDB for database hosting
+if (process.env.JAWSDB_URL) {
+
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+
+} else {
+
+    // using local environment variables 
+    sequelize = new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306,
+        }
+    );
+}
+
+
+
+// exports sequelize connection
+module.exports = sequelize;
---
+++ models/Recipe.js
@@ -0,0 +1,42 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// sets up Recipe Model
+class Recipe extends Model{};
+
+// initiates Recipe Model
+Recipe.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true,
+        },
+        recipe_text: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        main_ingredient: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        recipe_owner: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: "user",
+                key: "id"
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: true,
+        underscored: true,
+        modelName: "recipe",
+        freezeTableName: true
+    }
+);
+
+// exports Recipe Model
+module.exports = Recipe;
