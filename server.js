b56e03f91fad1848eed47d53d02e2f2aa051e757
mshaari <michael.shaari@gmail.com>
2023-02-24 10:21:58 -0500

    Fixing server.js file

--- server.js
+++ server.js
@@ -1,53 +1,46 @@
-const express = require('express')
-const { engine } = require('express-handlebars');
-const app = express();
-const PORT = process.env.PORT || 3001;
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const helpers = require('./utils/helpers');
 
-app.set('view engine', 'hbs');
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-app.engine('hbs', engine({
-    layoutsDir: `${__dirname}/views/layouts`,
-    extname: 'hbs',
-    defaultLayout: 'index',
-    partialsDir: `${__dirname}/views/partials`
-}));
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({ helpers });
 
 const sess = {
-    secret: 'Super secret secret',
-    cookie: {
-      maxAge: 60 * 60 * 1000,
-      httpOnly: true,
-      secure: false,
-      sameSite: 'strict',
-    },
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-      db: sequelize,
-    }),
+  secret: 'Super secret secret',
+  cookie: {
+    maxAge: 900000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
 };
-  
 
 app.use(session(sess));
 
+// Inform Express.js on which template engine to use
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'))
-
-app.get('/', (req, res) => {
-    res.render('main', {layout: 'index'});
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-app.listen(PORT, () =>
-    console.log(
-    `\nServer running on port ${PORT}. Visit http://localhost:${PORT}!`
-    )
-);
+  app.listen(PORT, () => console.log('Now listening'));
 });
