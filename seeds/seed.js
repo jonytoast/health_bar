64b6b400d320ace6eaa7539f680f12da220f9485
FeddericoWayne <jackiew1120@hotmail.com>
2023-03-03 09:16:32 -0500

    added comments to server.js, seeds directory, and nutritionRoutes.js

--- seeds/seed.js
+++ seeds/seed.js
@@ -10,16 +10,21 @@ const commentData = require('./commentSeedData.json');
 
 // async function to seed database
 const seedDatabase = async () => {
+
   await sequelize.sync({ force: true });
+  console.log("\n----- Database Synced -----\n");
 
   const users = await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
+  console.log("\n----- User Data Seeded -----\n");
 
   const recipes = await Recipe.bulkCreate(recipeData);
+  console.log("\n----- Recipe Data Seeded -----\n");
 
   const comments = await Comment.bulkCreate(commentData);
+  console.log("\n----- Comment Data Seeded -----\n");
 
   process.exit(0);
 };
